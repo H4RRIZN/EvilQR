9c81a796a6a40b9b80c6189c39f660d0a62b480f
Harrizzon <nicolas.ign19@gmail.com>
2022-01-12 22:35:01 -0300

    UPDATE

--- EvilQR.py
+++ EvilQR.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 import qrcode
+from alive_progress import alive_bar
+from time import sleep
+from os import system
+
 
 URL = input("Ingresa una URL: ")
 
@@ -14,4 +18,13 @@ qr.add_data(URL)
 qr.make(fit=True)
 
 img = qr.make_image(fill='black', back_color='white')
+
+print("Generando Codigo QR\n")
+
+# QR GENERATING
+with alive_bar(100, spinner='pulse') as progrs:
+    for i in range(100):
+        sleep(0.01)
+        progrs()
+system('clear')
 img.save('eQRCode.png')
