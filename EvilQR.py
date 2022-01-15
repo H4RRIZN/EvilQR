035c7c0c9d1c9a555708f4b7ec68a063e4c22da5
Harrizzon <nicolas.ign19@gmail.com>
2022-01-15 00:27:44 -0300

    Update

--- EvilQR.py
+++ EvilQR.py
@@ -42,33 +42,91 @@ signal.signal(signal.SIGINT, ctrl_c)
 if __name__ == '__main__':
     print(banner)
 
+    if len(sys.argv) == 1:
+        main_url = input("Enter an URL: ")
+        #Empty URL
+        if len(main_url) == 0:
 
-    URL = input("Ingresa una URL: \n")
+            print(style.RED + "[!]" + style.WHITE + "Enter a valid URL" + style.RESET)
+            sys.exit(1)
+        #URL
+        if len(main_url) == 1:
+            while True:
+                choose_mask = input(style.YELLOW + "¿do you want to mask the url? " + style.GREEN + "(y/m)\n" + style.RESET)
+                if choose_mask == "n":
+                    system('clear')
+                    qr = qrcode.QRCode(
+                    version=4,
+                    box_size=10,
+                    border=1
+                    )
+                    qr.add_data(main_url)
+                    qr.make(fit=True)
+                    img = qr.make_image(fill='black', back_color='white')
+                    print(style.YELLOW + "\nGenerando Codigo QR\n" + style.RESET)
+                    
+                    # QR GENERATING
+                    with alive_bar(100, spinner='pulse') as progrs:
+                        for i in range(100):
+                            sleep(0.01)
+                            progrs()
+                    system('clear')        
+                    print(style.GREEN + "\n\n[>] QR Generado con exito [<]" + style.RESET)
+                    print(style.GREEN + "Archivo almacenado en el repositorio: " + style.RESET + style.CYAN + "EvilQR/eQRCode.png " + style.RESET)
+                    sleep(4)
+                    system('clear')
+                    img.save('eQRCode.png')
+                    sys.exit(0)
+                if choose_mask == "y":
+                    system('clear')
+                    
 
-    # QR 
-    qr = qrcode.QRCode(
-    version=4,
-    box_size=10,
-    border=1
+
+
+
+
+                else:
+                    system('clear')
+                    print(style.RED + "\n [!] Please enter a valid option" + style.RESET)
+                    sleep(2)
+                    system('clear')
+
+    else:
+        print(style.RED + "[!]" + style.WHITE + "Invalid Argument" + style.RESET)
+        sys.exit(1)
+        
+    
+    
+    
+    
+    
+
+
+
+
+# QR 
+qr = qrcode.QRCode(
+version=4,
+box_size=10,
+border=1
 )
+qr.add_data(main_url)
+qr.make(fit=True)
+img = qr.make_image(fill='black', back_color='white')
 
+print(style.GREEN + "Generando Codigo QR\n")
 
-    qr.add_data(URL)
-    qr.make(fit=True)
+# QR GENERATING
+with alive_bar(100, spinner='pulse') as progrs:
+    for i in range(100):
+        sleep(0.01)
+        progrs()
+        system('clear')
+        print(style.GREEN + "\n[>] QR Generado con exito [<]" + style.RESET)
+        print(style.GREEN + "Archivo almacenado en el repositorio: " + style.RESET + style.CYAN + "EvilQR/eQRCode.png " + style.RESET)
+        sleep(5)
+        system('clear')
+        img.save('eQRCode.png')
 
-    img = qr.make_image(fill='black', back_color='white')
-
-    print(style.GREEN + "Generando Codigo QR\n")
-
-    # QR GENERATING
-    with alive_bar(100, spinner='pulse') as progrs:
-        for i in range(100):
-            sleep(0.01)
-            progrs()
-    system('clear')
-    print(style.GREEN + "\n[>] QR Generado con exito [<]" + style.RESET)
-    print(style.GREEN + "Archivo almacenado en el repositorio: " + style.RESET + style.CYAN + "EvilQR/eQRCode.png " + style.RESET)
-    sleep(5)
-    system('clear')
-    img.save('eQRCode.png')
+    
 
